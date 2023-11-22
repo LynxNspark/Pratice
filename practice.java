30b579bf246b158f1bc9746160e56588978a4764
LynxNspark <lynxnspark@gmail.com>
2023-11-22 14:32:55 -0800

    update1.0

--- practice.java
+++ practice.java
@@ -1,5 +1,5 @@
 public class practice {
     public static void main(String[] args) {
-        System.out.println("Hola mi amigo."); 
+        System.out.println("I can do this in my sleep."); 
     }
 }
